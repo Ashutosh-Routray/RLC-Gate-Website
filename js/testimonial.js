6255aa577ae3ab1018ab445073d70f0856060c00
frail <ashutosh.routray126@gamil.com>
2023-01-11 00:25:35 +0530

    try

---
+++ js/testimonial.js
@@ -0,0 +1,35 @@
+var slideIndex = 0;
+var cardDeck = document.querySelector(".card-deck");
+var testCard = document.getElementsByClassName("test-card");
+var cardLength = 0;
+var cards;
+
+storeCards();
+
+setInterval(() => { plus(1); }, 7000);
+
+function plus(n) {
+    showSlides(slideIndex += n);
+}
+
+function storeCards() {
+    cards = Object.entries(testCard);
+    // console.log(cards);
+    cardLength = cards.length;
+    // console.log(cards[0][1].outerHTML);
+    showSlides(slideIndex);
+    // console.log(cards);
+}
+
+function showSlides(n) {
+    if (n < 0) {
+        slideIndex = cardLength + n;
+    }
+    if (n >= cardLength) {
+        slideIndex = 0;
+    }
+    cardDeck.innerHTML = "";
+    cardDeck.innerHTML += cards[(slideIndex) % (cardLength)][1].outerHTML;
+    // cardDeck.innerHTML += cards[(slideIndex + 1) % (cardLength)][1].outerHTML;
+    // cardDeck.innerHTML += cards[(slideIndex + 2) % (cardLength)][1].outerHTML;
+}
